859eccd604735c3fd4c9018c607a3d606e4aee33
Artanisax <harrychen.kr@foxmail.com>
2026-05-12 13:49:14 +0800

    add: Win configure.bat

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ cmake -B Builds -G Ninja -DCMAKE_BUILD_TYPE=Release \
     -DKFR_ENABLE_MULTIARCH=OFF \
     -DKFR_ENABLE_DFT=ON \
     -DZL_JUCE_FORMATS="VST3;Standalone" .
-    # -DZL_JUCE_FORMATS="VST3;LV2" .
-cmake --build Builds --config Release -j$(nproc --ignore=8)
+cmake --build Builds --config Release -j 24
