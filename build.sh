2de3048a9b6abb82df551c7e93406a677f632ff5
Artanisax <harrychen.kr@foxmail.com>
2026-05-19 15:14:49 +0800

    delete: useless files

--- build.sh
+++
@@ -1,8 +0,0 @@
-git submodule update --init --recursive
-
-rm -rf Builds
-cmake -B Builds -G Ninja -DCMAKE_BUILD_TYPE=Release \
-    -DKFR_ENABLE_MULTIARCH=OFF \
-    -DKFR_ENABLE_DFT=ON \
-    -DZL_JUCE_FORMATS="VST3;Standalone" .
-cmake --build Builds --config Release -j 24
